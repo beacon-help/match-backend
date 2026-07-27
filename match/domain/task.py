4f12f8b3e1bbc78bbc4fdffa540112511aa5e596
julekb <julek.baranski@gmail.com>
2026-07-27 12:36:56 +0200

    Improve exceptions in domain layer

--- match/domain/task.py
+++ match/domain/task.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from datetime import timezone as tz
 from enum import StrEnum
 
+from match.domain.exceptions import InvalidTaskAction, NotAnOwner
 from match.domain.user import User, UserId
 
 
@@ -77,54 +78,74 @@ class Task:
 
     def _validate_owner(self, user: User) -> None:
         if self.owner_id != user.id:
-            raise Exception("User is not an owner.")
+            raise NotAnOwner("User is not an owner.")
 
     def join(self, helper_id: int) -> None:
         if self.status != TaskStatus.OPEN:
-            raise Exception(f"Cannot join this Task with status {self.status}")
+            raise InvalidTaskAction(f"Cannot join this Task with status {self.status}")
         if self.owner_id == helper_id:
-            raise Exception("Owner cannot join its own Task.")
+            raise InvalidTaskAction("Owner cannot join its own Task.")
         self.helper_id = helper_id
         self.status = TaskStatus.PENDING
         self._post_task_update()
 
-    def approve_helper(self, owner: User, helper_id: UserId) -> None:
-        self._validate_owner(owner)
+    def approve_helper(self, user: User, helper_id: UserId) -> None:
+        try:
+            self._validate_owner(user)
+        except NotAnOwner as e:
+            raise InvalidTaskAction from e
+
         if self.status != TaskStatus.PENDING or not self.helper_id:
-            raise Exception("Cannot approve helper.")
+            raise InvalidTaskAction("Cannot approve helper.")
         if self.helper_id != helper_id:
-            raise Exception(f"Incorrect helper_id {helper_id}.")
+            raise InvalidTaskAction(f"Incorrect helper_id {helper_id}.")
         self.status = TaskStatus.APPROVED
         self._post_task_update()
 
-    def reject_helper(self, owner: User, helper_id: UserId) -> None:
-        self._validate_owner(owner)
+    def reject_helper(self, user: User, helper_id: UserId) -> None:
+        try:
+            self._validate_owner(user)
+        except NotAnOwner as e:
+            raise InvalidTaskAction from e
+
         if self.status not in (TaskStatus.PENDING, TaskStatus.APPROVED):
-            raise Exception("Cannot reject helper.")
+            raise InvalidTaskAction("Cannot reject helper.")
         if self.helper_id is None:
-            raise Exception("No helper to reject.")
+            raise InvalidTaskAction("No helper to reject.")
         if self.helper_id != helper_id:
-            raise Exception(f"Incorrect helper_id {helper_id}")
+            raise InvalidTaskAction(f"Incorrect helper_id {helper_id}")
         self.status = TaskStatus.OPEN
         self.helper_id = None
         self._post_task_update()
 
     def report_succeeded(self, user: User) -> None:
-        self._validate_owner(user)
+        try:
+            self._validate_owner(user)
+        except NotAnOwner as e:
+            raise InvalidTaskAction from e
+
         if self.status != TaskStatus.APPROVED:
-            raise Exception("Cannot report this task.")
+            raise InvalidTaskAction("Cannot report this task.")
         self.status = TaskStatus.SUCCEEDED
         self._post_task_update()
 
     def report_failed(self, user: User) -> None:
-        self._validate_owner(user)
+        try:
+            self._validate_owner(user)
+        except NotAnOwner as e:
+            raise InvalidTaskAction from e
+
         if self.status != TaskStatus.APPROVED:
-            raise Exception("Cannot report this task.")
+            raise InvalidTaskAction("Cannot report this task.")
         self.status = TaskStatus.FAILED
         self._post_task_update()
 
     def close(self, user: User) -> None:
-        self._validate_owner(user)
+        try:
+            self._validate_owner(user)
+        except NotAnOwner as e:
+            raise InvalidTaskAction from e
+
         if self.status == TaskStatus.CANCELLED:
             raise Exception("Task already closed.")
         if self.status in (TaskStatus.SUCCEEDED, TaskStatus.FAILED):
